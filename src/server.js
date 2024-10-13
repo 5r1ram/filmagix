ca3ba6a4b57fbc46deae3ca031259db40dfb0270
5r1ram <sriramiiit33@gmail.com>
2024-10-13 13:58:48 -0400

    Resolved API Key Error

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
+import cors from "cors";
+
 const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, "build")));
 
